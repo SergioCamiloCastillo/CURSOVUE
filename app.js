d5e41b2dce86488f6e646860e07e9cfd939eaddb
SergioCamiloCastillo <scastillonu@uniminuto.edu.co>
2019-07-09 19:48:25 -0500

    Update app.js
    Persistencia de los datos

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
-const app=new Vue({
-    el:'#app',
+const app = new Vue({
+    el: '#app',
 
-    
-    data:{
-        titulo:'GYM con Vue',
-        tareas:[],
 
-        
-        nuevaTarea:''
+    data: {
+        titulo: 'GYM con Vue',
+        tareas: [],
+
+
+        nuevaTarea: ''
 
 
     },
-    methods:{
-        agregarPalabra(){
-           
+    methods: {
+        agregarPalabra() {
+
             this.tareas.push({
-                nombre:this.nuevaTarea,
-                status:false
+                nombre: this.nuevaTarea,
+                status: false
 
             });
-            this.nuevaTarea='';
+            this.nuevaTarea = '';
+            localStorage.setItem('gym-vue',JSON.stringify(this.tareas));
         },
-        editarTarea(index){
-            
-            this.tareas[index].status=true;
+        editarTarea(index) {
+
+            this.tareas[index].status = true;
+            localStorage.setItem('gym-vue',JSON.stringify(this.tareas));
+
         },
-        eliminarTarea(index){
-            this.tareas.splice(index,1);
+        eliminarTarea(index) {
+            this.tareas.splice(index, 1);
+            localStorage.setItem('gym-vue',JSON.stringify(this.tareas));
+
         }
-        
+
+    },
+    created() {
+        let datosDB = JSON.parse(localStorage.getItem('gym-vue'));
+        if (datosDB === null) {
+            this.tareas = [];
+
+        } else {
+            this.tareas = datosDB;
+        }
     }
 
 
